ae56433a7bab5eeacf847d1d5617227e6fd8e74b
appa12-tech <vsthapa12@gmail.com>
2021-07-11 11:45:44 +0545

    Widget

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String value = "Nothing now ";
+  void _onClick() => value = DateTime.now().toString();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Name Here"),
+      ),
+      body: Container(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              children: [
+                Text(value),
+                IconButton(onPressed: _onClick, icon: Icon(Icons.timer)),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
